8d92108a18e8cf51145b36d3024202cd07e3cb41
Melvin Focke <79911517+melvinfocke@users.noreply.github.com>
2024-07-20 11:25:53 +0200

    Add env var to show or hide StyleSwitcher

--- src/env.ts
+++ src/env.ts
@@ -6,3 +6,4 @@ export const instagramLink = 'https://www.instagram.com/fm_veranstaltungstechnik
 //export const addressCondesed = `Fabian Bartsch<br />FM&nbsp;Veran&shy;staltungs&shy;technik<br />An der Ziegelei 4, 14959 Kliestow`;
 //export const addressFull = `Fabian Bartsch<br />FM&nbsp;Veran&shy;staltungs&shy;technik<br />An der Ziegelei 4<br />14959 Kliestow`;
 export const site = 'https://www.fm-veranstaltungstechnik.de';
+export const showStyleSwitcher = false;
